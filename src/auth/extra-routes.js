adab21334be31aea1147d64f0b6a47fc2044101b
fatemaowedah <fatema.owedah@gmail.com>
2020-06-11 17:16:20 +0300

    add

---
+++ src/auth/extra-routes.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const express = require('express');
+const authRouter = express.Router();
+const Users = require('./models/users-model');
+
+
+module.exports =  authRouter;
---
+++ src/auth/middleware/bearer.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const users = require('../models/users-model');
+
+module.exports = (req, res, next) => {
+
+  if (!req.headers.authorization) {
+    next('User is not loggedin');
+    return;
+  }
+
+  
+  console.log('req.headers.authorization >>>> ',req.headers.authorization);
+  let bearerToken = req.headers.authorization.split(' ').pop();
+
+  users.verifyToken(bearerToken)
+    .then(decodedUserObject => {
+      req.user = decodedUserObject;
+      next();
+    }).catch(err=> next('Protected: Invalid User Token'));
+    
+};
