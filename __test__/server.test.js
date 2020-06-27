fb96662f2196498ecaf07a4fbdf13f2f69490f39
fatemaowedah <fatema.owedah@gmail.com>
2020-06-27 10:03:10 +0300

    add more test

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -7,32 +7,10 @@ jest.spyOn(global.console, 'log');
 const mockRequest = supergoose(server);
 
 describe('web server',()=>{
-
-  it('should respond with 500',()=>{
-    return mockRequest.get('/bad')
-      .then(results=> {
-        expect(results.status).toBe(500);
-      }).catch(console.error);
-  });
-  it('not Found',()=>{
-    return mockRequest
-      .get('/wrongpath')
-      .then(result=>{
-        expect(result.status).toEqual(404);
-      });
-  });
   let user={
     username:'fatema@test.com',
     password:'1234',
   };
-  it('test route /users', ()=> {
-    return mockRequest
-      .get('/users')
-      .then(results => {
-        expect(results.status).toBe(200);
-      });
-  });
-
   it('test route  /signup ', async() => {
     mockRequest
       .post('/signup')
